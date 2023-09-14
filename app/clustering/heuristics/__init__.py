a265e775a87eb1e5dc8a9314e5d7f6b75cac646c
David <david.levin2@live.biu.ac.il>
2023-09-14 10:15:06 +0300

    Delete unused code

--- app/clustering/heuristics/__init__.py
+++ app/clustering/heuristics/__init__.py
@@ -1,9 +1,7 @@
-from .sa_heuristic import SAHeuristic
 from .basic_heuristic import BasicHeuristic
 from .greedy_heuristic import GreedyHeuristic
 
 __all__ = [
-    SAHeuristic,
     BasicHeuristic,
     GreedyHeuristic
 ]
